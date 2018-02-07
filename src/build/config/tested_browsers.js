50e38ba3a2081fb6581a7ce0ff90db4f23315946
chooie <chebert@spotx.tv>
2018-02-07 16:02:30 -0700

    Update tested browsers

--- src/build/config/tested_browsers.js
+++ src/build/config/tested_browsers.js
@@ -2,13 +2,12 @@
   "use strict";
 
   module.exports = [
-    "IE 11.0.0 (Windows 7 0.0.0)",
-    "Edge 14.14393.0 (Windows 10 0.0.0)",
-    "Firefox 57.0.0 (Mac OS X 10.13.0)",
     "Chrome 63.0.3239 (Mac OS X 10.13.2)",
+    "Chrome Mobile 55.0.2883 (Android 7.1.1)",
+    "Edge 16.16299.0 (Windows 10.0.0)",
+    "Firefox 57.0.0 (Mac OS X 10.13.0)",
     "Safari 11.0.2 (Mac OS X 10.13.2)",
     "Mobile Safari 11.0.0 (iOS 11.2.0)",
-    "Chrome Mobile WebView 44.0.2403 (Android 6.0.0)"
   ];
 
 }());
