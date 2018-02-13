76dafa8e2d433c6631418c9dc201f7e7b8aa6578
chooie <chebert@spotx.tv>
2018-02-13 10:21:04 -0700

    Bump browser versions

--- src/build/config/tested_browsers.js
+++ src/build/config/tested_browsers.js
@@ -2,10 +2,10 @@
   "use strict";
 
   module.exports = [
-    "Chrome 63.0.3239 (Mac OS X 10.13.2)",
+    "Chrome 64.0.3282 (Mac OS X 10.13.2)",
     "Chrome Mobile 55.0.2883 (Android 7.1.1)",
     "Edge 16.16299.0 (Windows 10.0.0)",
-    "Firefox 57.0.0 (Mac OS X 10.13.0)",
+    "Firefox 58.0.0 (Mac OS X 10.13.0)",
     "Safari 11.0.2 (Mac OS X 10.13.2)",
     "Mobile Safari 11.0.0 (iOS 11.2.0)",
   ];
