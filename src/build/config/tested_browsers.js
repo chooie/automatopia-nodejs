de21b75109b7416b43ec8989825dadebc31459c7
chooie <charlie.hebert92@gmail.com>
2018-03-24 16:56:32 -0600

    Bump Firefox 58.0.0 -> 59.0.0

--- src/build/config/tested_browsers.js
+++ src/build/config/tested_browsers.js
@@ -5,7 +5,7 @@
     "Chrome 65.0.3325 (Mac OS X 10.13.3)",
     "Chrome Mobile 55.0.2883 (Android 7.1.1)",
     "Edge 16.16299.0 (Windows 10.0.0)",
-    "Firefox 58.0.0 (Mac OS X 10.13.0)",
+    "Firefox 59.0.0 (Mac OS X 10.13.0)",
     "Safari 11.0.3 (Mac OS X 10.13.3)",
     "Mobile Safari 11.0.0 (iOS 11.2.0)",
   ];
