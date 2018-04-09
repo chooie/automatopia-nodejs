9dcd8c03516bb990a4f449b09f5711791b204b4a
chooie <chebert@spotx.tv>
2018-04-09 13:29:11 -0600

    Local and docker container now share the same node_modules and dir and rebuild binaries as needed

--- docker-tasks.sh
+++ docker-tasks.sh
@@ -13,29 +13,20 @@ if [ $@ == 'karma' ]; then
          --tty \
          --interactive \
          --publish 9876:9876 \
-         --volume=`pwd`/generated/:/usr/src/app/generated/ \
-         --volume=`pwd`/src/:/usr/src/app/src/ \
-         --volume=`pwd`/Procfile:/usr/src/app/Procfile \
-         --volume=`pwd`/tasks.sh:/usr/src/app/tasks.sh \
+         --volume=`pwd`:/usr/src/app/ \
          'automatopia-nodejs' "$THIS_DIR/tasks.sh" $@
 elif [ $@ == 'run' ]; then
   docker run \
          --tty \
          --interactive \
          --publish 5000:5000 \
-         --volume=`pwd`/generated/:/usr/src/app/generated/ \
-         --volume=`pwd`/src/:/usr/src/app/src/ \
-         --volume=`pwd`/Procfile:/usr/src/app/Procfile \
-         --volume=`pwd`/tasks.sh:/usr/src/app/tasks.sh \
+         --volume=`pwd`:/usr/src/app/ \
          'automatopia-nodejs' "$THIS_DIR/tasks.sh" $@
 else
     docker run \
            --tty \
            --interactive \
            --net="host" \
-           --volume=`pwd`/generated/:/usr/src/app/generated/ \
-           --volume=`pwd`/src/:/usr/src/app/src/ \
-           --volume=`pwd`/Procfile:/usr/src/app/Procfile \
-           --volume=`pwd`/tasks.sh:/usr/src/app/tasks.sh \
+           --volume=`pwd`:/usr/src/app/ \
            'automatopia-nodejs' "$THIS_DIR/tasks.sh" $@
 fi
--- tasks.sh
+++ tasks.sh
@@ -5,4 +5,17 @@ if [ ! -f node_modules/chromedriver/lib/chromedriver/chromedriver ]; then
    npm rebuild
 fi
 
+mkdir -p generated_build
+built_operating_system=`cat generated_build/operating_system.txt`
+current_operating_system=`uname`
+
+if [ $built_operating_system != `uname` ]; then
+  echo $current_operating_system > generated_build/operating_system.txt
+  echo "New platform detected. Was ${built_operating_system}, now \
+${current_operating_system}"
+
+  echo 'Rebuilding binaries...'
+  npm rebuild
+fi
+
 ./node_modules/jake/bin/cli.js --jakefile src/build/Jakefile.js $@
