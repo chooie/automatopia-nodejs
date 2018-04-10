ff44a06fa3080b9a8640fdd63c4b7ddbb7d2115f
chooie <chebert@spotx.tv>
2018-04-10 13:15:04 -0600

    Remove iife in _run_server.js

--- src/application/_run_server.js
+++ src/application/_run_server.js
@@ -1,46 +1,42 @@
-(function() {
-  "use strict";
+const child_process = require("child_process");
+const fs = require("fs");
+const procfile = require("procfile");
 
-  const child_process = require("child_process");
-  const fs = require("fs");
-  const procfile = require("procfile");
+const config = require("../application/shared/config.js");
 
-  const config = require("../application/shared/config.js");
+exports.runInteractively = function() {
+  return run("inherit");
+};
 
-  exports.runInteractively = function() {
-    return run("inherit");
-  };
+exports.runProgrammatically = function(callback) {
+  const serverProcess = runOnlyWithStdErr();
 
-  exports.runProgrammatically = function(callback) {
-    const serverProcess = runOnlyWithStdErr();
+  serverProcess.stdout.setEncoding("utf8");
+  serverProcess.stdout.on("data", function(chunk) {
+    if (chunk.trim().indexOf("Server started") !== -1) {
+      callback(serverProcess);
+    }
+  });
+};
 
-    serverProcess.stdout.setEncoding("utf8");
-    serverProcess.stdout.on("data", function(chunk) {
-      if (chunk.trim().indexOf("Server started") !== -1) {
-        callback(serverProcess);
-      }
-    });
-  };
+function runOnlyWithStdErr() {
+  const serverProcess = run(["pipe", "pipe", process.stderr]);
+  return serverProcess;
+}
 
-  function runOnlyWithStdErr() {
-    const serverProcess = run(["pipe", "pipe", process.stderr]);
-    return serverProcess;
-  }
+function run(stdioOptions) {
+  const commandLine = parseProcFile();
+  return child_process.spawn(commandLine.command, commandLine.options, {
+    stdio: stdioOptions
+  });
+}
 
-  function run(stdioOptions) {
-    const commandLine = parseProcFile();
-    return child_process.spawn(commandLine.command, commandLine.options, {
-      stdio: stdioOptions
-    });
-  }
-
-  function parseProcFile() {
-    const fileData = fs.readFileSync("Procfile", "utf8");
-    const webCommand = procfile.parse(fileData).web;
-    webCommand.options = webCommand.options.map(function(element) {
-      if (element === "$PORT") return config.defaultPort;
-      else return element;
-    });
-    return webCommand;
-  }
-})();
+function parseProcFile() {
+  const fileData = fs.readFileSync("Procfile", "utf8");
+  const webCommand = procfile.parse(fileData).web;
+  webCommand.options = webCommand.options.map(function(element) {
+    if (element === "$PORT") return config.defaultPort;
+    else return element;
+  });
+  return webCommand;
+}
