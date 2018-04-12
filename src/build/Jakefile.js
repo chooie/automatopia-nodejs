2975ed42e20deba7c12f0f3efc21ff81b1f67de3
chooie <chebert@spotx.tv>
2018-04-12 14:31:03 -0600

    Move lint task to its own module

--- src/build/Jakefile.js
+++ src/build/Jakefile.js
@@ -56,8 +56,9 @@ task(
   { async: true }
 );
 
-require("./tasks/test.js");
 require("./tasks/build_distribution.js");
+require("./tasks/lint.js");
+require("./tasks/test.js");
 
 desc("Delete all generated files");
 task("clean", [], function() {
@@ -104,46 +105,8 @@ task(
   { async: true }
 );
 
-//*** LINT
-
-desc("Lint everything");
-task("lint", ["lintLog", "incrementalLint"], function() {
-  console.log();
-});
-
-task("lintLog", function() {
-  process.stdout.write("Linting JavaScript: ");
-});
-
-task("incrementalLint", paths.lintDirectories());
-task("incrementalLint", paths.lintOutput());
-createDirectoryDependencies(paths.lintDirectories());
-
-rule(".lint", determineLintDependency, function() {
-  const lint = require("./lint_runner.js");
-  const lintConfig = require("./config/eslint.conf.js");
-
-  const passed = lint.validateFile(this.source, lintConfig.options);
-  if (passed) fs().writeFileSync(this.name, "lint ok");
-  else fail("Lint failed");
-});
-function determineLintDependency(name) {
-  const result = name.replace(/^generated\/incremental\/lint\//, "");
-  return result.replace(/\.lint$/, "");
-}
-
-function createDirectoryDependencies(directories) {
-  directories.forEach(function(lintDirectory) {
-    directory(lintDirectory);
-  });
-}
-
 //*** LAZY-LOADED MODULES
 
 function karmaRunner() {
   return require("simplebuild-karma");
-}
-
-function fs() {
-  return require("fs");
 }
