e27bd90f1d3b339d2942b8e1c6fae48141d6f9e4
chooie <charlie.hebert92@gmail.com>
2018-06-25 11:05:09 -0600

    Bump browsers

--- src/build/config/tested_browsers.js
+++ src/build/config/tested_browsers.js
@@ -1,8 +1,8 @@
 module.exports = [
-  "Chrome 66.0.3359 (Mac OS X 10.13.3)",
-  "Chrome Mobile 55.0.2883 (Android 7.1.1)",
-  "Edge 16.16299.0 (Windows 10.0.0)",
-  "Firefox 59.0.0 (Mac OS X 10.13.0)",
+  "Chrome 67.0.3396 (Linux 0.0.0)",
+  "Chrome Mobile 67.0.3396 (Android 8.1.0)",
+  "Edge 17.17134.0 (Windows 10.0.0)",
+  "Firefox 60.0.0 (Ubuntu 0.0.0)",
   "Safari 11.0.3 (Mac OS X 10.13.3)",
-  "Mobile Safari 11.0.0 (iOS 11.2.0)"
+  "Mobile Safari 11.0.0 (iOS 11.4.0)"
 ];
